5119e1f1ae3e089d2a8f2d27e6d79949b36b3ed9
Rodriguez-Francisco <chiscorodriguez3010@gmail.com>
2025-09-19 06:49:13 -0600

    test commit DLL

--- DL-List.hpp
+++ DL-List.hpp
@@ -3,3 +3,28 @@ UWYO COSC2030
 Graded lab 2
 */
 #pragma once
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node* prev;
+};
+
+class DLL{
+    private:
+    Node* head;
+    Node*tail;
+
+public:
+    DLL();
+    void insertAtHead(int val);
+    void insertAttail(int val);
+
+    void deleteAtHead();
+    void deleteAtTail();
+    void traverse();
+    ~DLL();
+
+
+};
